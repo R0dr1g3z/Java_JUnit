07f3cf69503449b9cdda961d62670d3fba72e2bf
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-02-06 21:15:54 +0100

    add task4 junit

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static boolean greater(int a, int b) {
+        return a > b;
+    }
+}
---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        assertEquals(15, Calculator.add(10, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5, Calculator.divide(25, 5));
+    }
+
+    @Test
+    public void testGreater() {
+        assertTrue(Calculator.greater(5, 2));
+        assertFalse(Calculator.greater(2, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, Calculator.multiply(5, 10));
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(5, Calculator.sub(10, 5));
+    }
+}
