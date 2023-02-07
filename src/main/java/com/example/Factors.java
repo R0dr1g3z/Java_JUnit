9e54602075e92a8b0e0fc0486ccd4d04e82e9c8a
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-02-07 05:59:13 +0100

    add task2 tdd step2

--- src/main/java/com/example/Factors.java
+++ src/main/java/com/example/Factors.java
@@ -1,10 +1,17 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Factors {
     
     public static List<Integer> generatePrimeFactors(int n){
-        return null;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0){
+                list.add(i);
+            }
+        }
+        return list;
     }
 }
